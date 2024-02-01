e2a6b20aea706dc92576c0134c72bb5453a67cfa
Vladimir Jakubik <vladimir.jakubik77@gmail.com>
2024-02-01 15:02:30 +0100

    Change css of buttons and footer-text

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import express from "express";
-import data from blogPost;
-
 
 const port = 3000;
 const app = express();
-var data = data();
-console.log(data.length);
+
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", data);
+    res.render("index.ejs");
 });
 
 app.listen(port, () => {
