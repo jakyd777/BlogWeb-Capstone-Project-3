866b09e7afc547b84c2b33d44a3e3fc46f492fa0
Vladimir Jakubik <vladimir.jakubik77@gmail.com>
2024-02-01 19:18:11 +0100

    Delete blogPost.js and modify Index.js and index.ejs add post.ejs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const port = 3000;
 const app = express();
@@ -6,10 +7,32 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {posts: data});
+});
+
+app.post("/article", (req, res) => {
+    let postNumber = req.body.postNum;
+    res.render("post.ejs", data[postNumber]);
 });
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+let data = [
+    {
+        title: "First Post on Blog",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        author: "Vladimir Jakubik",
+    },
+    {
+        title: "Second Post on Blog",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        author: "Gabriela Jakubikova",
+    },
+]
