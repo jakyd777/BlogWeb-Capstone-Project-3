4c9c5ab9680853f21d0ae3de82814bacf2d18872
Vladimir Jakubik <vladimir.jakubik77@gmail.com>
2024-02-01 12:11:34 +0100

    Deleted blogPosts files Modify index.js and index.ejs

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
+import data from blogPost;
+
 
 const port = 3000;
 const app = express();
+var data = data();
+console.log(data.length);
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", data);
 });
 
 app.listen(port, () => {
